c357b3007e34c7e950a1cbdcf27d67b3ce49f86c
root <georgeongit@gmail.com>
2025-01-22 13:02:43 +0000

    app(add): add generic with all base CRUD operation

--- app/main.py
+++ app/main.py
@@ -1,11 +1,14 @@
 import asyncio
 
 from app.dao.base import engine, Base
+from app.dao.generic import BigGeneric
 
 
 async def main():
     async with engine.begin() as conn:
         # Ваш код здесь
         await conn.run_sync(Base.metadata.create_all)
+
+    BG = BigGeneric()
 
 asyncio.run(main())
