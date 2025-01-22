ca14a1ba851ce26d22f4249b6372e1c7de7f31b9
root <georgeongit@gmail.com>
2025-01-22 13:33:08 +0000

    app(add): add user api point

--- app/api/routers.py
+++ app/api/routers.py
@@ -1,5 +1,9 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.base import db
+from app.schemas import User
+from app.generic import BigGeneric
 
 router = APIRouter()
 
@@ -8,3 +12,7 @@ router = APIRouter()
 async def home_page():
     return {
         "message": "Hello. My name is Captain Capital"}
+
+@router.get("/users")
+async def get_users(session: AsyncSession = Depends(db.get_db)):
+    return await BigGeneric(User).find_all(session=session)
